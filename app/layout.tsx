833eab38fa9708fd0c9543c5f3f7dbe780c30249
rifqimubarok430 <rifqimubarok430@gmail.com>
2024-10-25 10:51:53 +0700

    first-commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,14 @@
+import "@/app/ui/global.css";
+import { inter } from "@/app/ui/fonts";
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="id" className="translated-ltr">
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
